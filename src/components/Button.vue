ecb843c3293d5daec5bcb5efc238579659c594c1
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-08-13 12:57:52 +0100

    :tada: first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,5 @@
+<template>
+    <button class="bg-[#23af23] hover:bg-green-500 duration-300 font-semibold text-white rounded py-2.5 px-12">
+        <slot></slot>
+    </button>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        DarkerGrotesque: ["Darker Grotesque", "sans-serif"],
+      },
+      boxShadow: {
+        'custom': '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+      },
+    },
+  },
+  plugins: [],
+}
