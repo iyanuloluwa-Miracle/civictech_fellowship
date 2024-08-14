444ed5513a431a20722ffbc4062785eaafeb22dc
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-08-14 08:08:36 +0100

    :sparkles: feat: Added content inside the card element

--- src/sections/Features.vue
+++ src/sections/Features.vue
@@ -1,18 +1,55 @@
 <template>
-    <section class="bg-[#228B22] text-center">
-        <h1 class="text-[2.9rem] mt-9 text-[white] font-extrabold lg:mb-5 pt-2 lg:text-5xl">Features</h1>
+    <section class="bg-[#228B22] text-center py-10">
+        <h1 class="text-[2.9rem] mt-9 text-white font-extrabold lg:mb-5 pt-2 lg:text-5xl">Features</h1>
 
-
-        <p class="text-[19px] font-semibold text-gray-900">
+        <p class="text-[19px] font-semibold text-gray-100 max-w-2xl mx-auto mb-10">
             Our platform allows users to report pollution quickly with location details and photos, enabling fast action
-            and greater community involvement
+            and greater community involvement.
         </p>
 
+        <div class="container mx-auto">
+            <section class="flex flex-wrap items-center justify-center gap-10">
+
+
+                <div class="bg-white p-10 rounded-md shadow-md hover:bg-TiffanyBlue lg:max-w-sm">
+                    <img src="../../assets/icons/barcode.svg" alt="barcode" width="35" class="mb-5 mx-auto" />
+                    <h4 class="text-2xl font-bold mb-5 lg:text-4xl lg:max-w-xs mx-auto">
+                        Instant Pollution Reporting
+                    </h4>
+                    <p class="leading-normal text-deepTeal font-semibold">
+                        Users can quickly report pollution by marking locations, adding details, and uploading photos
+                        for prompt action.
+                    </p>
+                </div>
+
+                <div class="bg-white p-10 rounded-md shadow-md hover:bg-TiffanyBlue lg:max-w-sm">
+                    <img src="../../assets/icons/barcode.svg" alt="barcode" width="35" class="mb-5 mx-auto" />
+                    <h4 class="text-2xl font-bold mb-5 lg:text-4xl lg:max-w-xs mx-auto">
+                        Interactive Map Visualization
+                    </h4>
+                    <p class="leading-normal text-deepTeal font-semibold">
+                        Our platform displays all submitted reports on an interactive map, providing a clear view of
+                        affected areas and helping authorities prioritize responses.
+                    </p>
+                </div>
+
+                <div class="bg-white p-10 rounded-md shadow-md hover:bg-TiffanyBlue lg:max-w-sm">
+                    <img src="../../assets/icons/user.svg" alt="user" width="35" class="mb-5 mx-auto" />
+                    <h4 class="text-2xl font-bold mb-5 lg:text-4xl lg:max-w-xs mx-auto">
+                        Real-Time Notifications
+                    </h4>
+                    <p class="leading-normal text-deepTeal font-semibold">
+                        Receive real-time notifications about the status of your reports, keeping you connected and
+                        engaged in the process of environmental protection.
+                    </p>
+                </div>
+
+
+
+            </section>
+        </div>
     </section>
-
-
 </template>
-
 
 <script>
 
