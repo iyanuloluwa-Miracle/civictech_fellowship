b57980cf45655880e04078cfaee36d84b9129e81
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-08-14 08:23:23 +0100

    :sparkles: feat: Added content inside the card element

--- src/sections/Features.vue
+++ src/sections/Features.vue
@@ -12,33 +12,33 @@
 
 
                 <div class="bg-white p-10 rounded-md shadow-md hover:bg-TiffanyBlue lg:max-w-sm">
-                    <img src="../../assets/icons/barcode.svg" alt="barcode" width="35" class="mb-5 mx-auto" />
+                    <img src="../assets/pollution.svg" alt="barcode" width="35" class="mb-5 mx-auto" />
                     <h4 class="text-2xl font-bold mb-5 lg:text-4xl lg:max-w-xs mx-auto">
                         Instant Pollution Reporting
                     </h4>
-                    <p class="leading-normal text-deepTeal font-semibold">
+                    <p class="leading-normal text-gray-900  font-semibold">
                         Users can quickly report pollution by marking locations, adding details, and uploading photos
                         for prompt action.
                     </p>
                 </div>
 
                 <div class="bg-white p-10 rounded-md shadow-md hover:bg-TiffanyBlue lg:max-w-sm">
-                    <img src="../../assets/icons/barcode.svg" alt="barcode" width="35" class="mb-5 mx-auto" />
+                    <img src="../assets/map 1.svg" alt="barcode" width="35" class="mb-5 mx-auto" />
                     <h4 class="text-2xl font-bold mb-5 lg:text-4xl lg:max-w-xs mx-auto">
                         Interactive Map Visualization
                     </h4>
-                    <p class="leading-normal text-deepTeal font-semibold">
+                    <p class="leading-normal text-gray-900  font-semibold">
                         Our platform displays all submitted reports on an interactive map, providing a clear view of
                         affected areas and helping authorities prioritize responses.
                     </p>
                 </div>
 
                 <div class="bg-white p-10 rounded-md shadow-md hover:bg-TiffanyBlue lg:max-w-sm">
-                    <img src="../../assets/icons/user.svg" alt="user" width="35" class="mb-5 mx-auto" />
+                    <img src="../assets/notification 1.svg" alt="user" width="35" class="mb-5 mx-auto" />
                     <h4 class="text-2xl font-bold mb-5 lg:text-4xl lg:max-w-xs mx-auto">
                         Real-Time Notifications
                     </h4>
-                    <p class="leading-normal text-deepTeal font-semibold">
+                    <p class="leading-normal text-gray-900 font-semibold">
                         Receive real-time notifications about the status of your reports, keeping you connected and
                         engaged in the process of environmental protection.
                     </p>
