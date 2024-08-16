61b9293affa9aa3484c56fe1f0d48f5263c399d7
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-08-16 21:32:37 +0100

    :sparkles: feat:coming soon ui redesign

--- src/page/ComingSoon.vue
+++ src/page/ComingSoon.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col md:flex-row items-center flex-wrap py-5 px-[7%] min-h-screen gap-4 pb-20">
+    <div class="flex flex-col md:flex-row items-center flex-wrap py-9 px-[7%] min-h-screen gap-4 pb-20">
         <div class="w-full md:flex-1 min-w-[250px] text-center md:text-left">
             <h3 class="text-[35px] md:text-[45px] uppercase text-[#228B22] font-extrabold">Coming Soon</h3>
 
@@ -8,30 +8,30 @@
                 difference in bringing it to life. Stay tuned for more updates!
             </p>
 
-            <div class="flex flex-wrap justify-center md:justify-start gap-4 py-2">
-                <div class="flex-1 p-2 bg-gray-200 border border-gray-800 text-center max-w-[120px]">
+            <div class="flex flex-wrap justify-center md:justify-start gap-4 py-7">
+                <div class="flex-1 p-2 bg-gray-200 border  border-[#23af23] text-center max-w-[120px]">
                     <h3 class="text-[30px] md:text-[50px] text-gray-800">{{ days }}</h3>
-                    <span class="text-[#228B22] text-[20px] font-semibold">Day</span>
+                    <span class="text-[#228B22] text-[20px] font-bold">Day</span>
                 </div>
 
-                <div class="flex-1 p-2 bg-gray-200 border border-gray-800 text-center max-w-[120px]">
+                <div class="flex-1 p-2 bg-gray-200 border  border-[#23af23] text-center max-w-[120px]">
                     <h3 class="text-[30px] md:text-[50px] text-gray-800">{{ hours }}</h3>
-                    <span class="text-[#228B22] text-[20px] font-semibold">Hour</span>
+                    <span class="text-[#228B22] text-[20px] font-bold">Hour</span>
                 </div>
 
-                <div class="flex-1 p-2 bg-gray-200 border border-gray-800 text-center max-w-[120px]">
+                <div class="flex-1 p-2 bg-gray-200 border border-[#23af23] text-center max-w-[120px]">
                     <h3 class="text-[30px] md:text-[50px] text-gray-800">{{ minutes }}</h3>
-                    <span class="text-blue-500 text-[20px]">Minute</span>
+                    <span class="text-[#228B22] font-bold text-[20px]">Minute</span>
                 </div>
 
-                <div class="flex-1 p-2 bg-gray-200 border border-gray-800 text-center max-w-[120px]">
+                <div class="flex-1 p-2 bg-gray-200 border  border-[#23af23] text-center max-w-[120px]">
                     <h3 class="text-[30px] md:text-[50px] text-gray-800">{{ seconds }}</h3>
-                    <span class="text-blue-500 text-[20px]">Second</span>
+                    <span class="font-bold text-[#228B22] text-[20px]">Second</span>
                 </div>
             </div>
 
             <a href="#"
-                class="inline-block py-2 px-6 md:px-12 mt-2 text-[14px] md:text-[17px] bg-blue-500 text-white transition-all hover:tracking-wider">
+                class="inline-block py-2 px-6 md:px-12 mt-2 text-[14px] md:text-[17px] bg-[#23af23] text-white transition-all hover:tracking-wider hover:bg-green-500">
                 Notify me
             </a>
         </div>
