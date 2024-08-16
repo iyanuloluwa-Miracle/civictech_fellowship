0dc0269a8828e393fe33f65d50adb2d1ae3cc6d0
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-08-16 16:16:47 +0100

    :sparkles: feat:coming soon ui redesign

--- src/page/ComingSoon.vue
+++ src/page/ComingSoon.vue
@@ -1,42 +1,42 @@
 <template>
-    <div class="flex items-center flex-wrap py-5 px-[7%] min-h-screen gap-4 pb-20">
-        <div class="flex-1 min-w-[400px]">
-            <h3 class="text-[45px] uppercase text-blue-500">Coming Soon</h3>
+    <div class="flex flex-col md:flex-row items-center flex-wrap py-5 px-[7%] min-h-screen gap-4 pb-20">
+        <div class="w-full md:flex-1 min-w-[250px] text-center md:text-left">
+            <h3 class="text-[35px] md:text-[45px] uppercase text-[#228B22] font-extrabold">Coming Soon</h3>
 
-            <p class="py-1 text-blue-500">
+            <p class="py-1 text-gray-900 font-semibold text-sm md:text-base">
                 We're dedicated to building this platform for you, and your financial support can make a significant
                 difference in bringing it to life. Stay tuned for more updates!
             </p>
 
-            <div class="flex gap-4 py-2">
-                <div class="flex-1 p-2 bg-gray-200 border border-gray-800 text-center">
-                    <h3 class="text-[50px] text-gray-800">{{ days }}</h3>
-                    <span class="text-blue-500 text-[20px]">Day</span>
+            <div class="flex flex-wrap justify-center md:justify-start gap-4 py-2">
+                <div class="flex-1 p-2 bg-gray-200 border border-gray-800 text-center max-w-[120px]">
+                    <h3 class="text-[30px] md:text-[50px] text-gray-800">{{ days }}</h3>
+                    <span class="text-[#228B22] text-[20px] font-semibold">Day</span>
                 </div>
 
-                <div class="flex-1 p-2 bg-gray-200 border border-gray-800 text-center">
-                    <h3 class="text-[50px] text-gray-800">{{ hours }}</h3>
-                    <span class="text-blue-500 text-[20px]">Hour</span>
+                <div class="flex-1 p-2 bg-gray-200 border border-gray-800 text-center max-w-[120px]">
+                    <h3 class="text-[30px] md:text-[50px] text-gray-800">{{ hours }}</h3>
+                    <span class="text-[#228B22] text-[20px] font-semibold">Hour</span>
                 </div>
 
-                <div class="flex-1 p-2 bg-gray-200 border border-gray-800 text-center">
-                    <h3 class="text-[50px] text-gray-800">{{ minutes }}</h3>
+                <div class="flex-1 p-2 bg-gray-200 border border-gray-800 text-center max-w-[120px]">
+                    <h3 class="text-[30px] md:text-[50px] text-gray-800">{{ minutes }}</h3>
                     <span class="text-blue-500 text-[20px]">Minute</span>
                 </div>
 
-                <div class="flex-1 p-2 bg-gray-200 border border-gray-800 text-center">
-                    <h3 class="text-[50px] text-gray-800">{{ seconds }}</h3>
+                <div class="flex-1 p-2 bg-gray-200 border border-gray-800 text-center max-w-[120px]">
+                    <h3 class="text-[30px] md:text-[50px] text-gray-800">{{ seconds }}</h3>
                     <span class="text-blue-500 text-[20px]">Second</span>
                 </div>
             </div>
 
             <a href="#"
-                class="inline-block py-2 px-12 mt-2 text-[17px] bg-blue-500 text-white transition-all hover:tracking-wider">
+                class="inline-block py-2 px-6 md:px-12 mt-2 text-[14px] md:text-[17px] bg-blue-500 text-white transition-all hover:tracking-wider">
                 Notify me
             </a>
         </div>
 
-        <div class="flex-1 min-w-[400px]">
+        <div class="w-full md:flex-1 min-w-[250px] mt-5 md:mt-0">
             <img src="../assets/pageconstruction.svg" alt="coming-soon image" class="w-full" />
         </div>
     </div>
@@ -53,46 +53,22 @@ const seconds = ref(0);
 const countDate = new Date('Dec 1, 2024 00:00:00').getTime();
 
 const countDown = () => {
-  const now = new Date().getTime();
-  const gap = countDate - now;
+    const now = new Date().getTime();
+    const gap = countDate - now;
 
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
 
-  days.value = Math.floor(gap / day);
-  hours.value = Math.floor((gap % day) / hour);
-  minutes.value = Math.floor((gap % hour) / minute);
-  seconds.value = Math.floor((gap % minute) / second);
+    days.value = Math.floor(gap / day);
+    hours.value = Math.floor((gap % day) / hour);
+    minutes.value = Math.floor((gap % hour) / minute);
+    seconds.value = Math.floor((gap % minute) / second);
 };
 
 onMounted(() => {
-  countDown();
-  setInterval(countDown, 1000);
+    countDown();
+    setInterval(countDown, 1000);
 });
 </script>
-
-<style scoped>
-@media (max-width: 500px) {
-  .container .title {
-    font-size: 35px;
-  }
-
-  .container .content p {
-    font-size: 14px;
-  }
-
-  .container .content .count-down {
-    gap: 5px;
-  }
-
-  .container .content .count-down h3 {
-    font-size: 30px;
-  }
-
-  .container .content .count-down span {
-    font-size: 15px;
-  }
-}
-</style>
